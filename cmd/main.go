e3e8f470bcdf900350a82af605ad6694322b7b61
Raunit Verma <ramgopal10258@gmail.com>
2024-05-08 22:50:20 +0530

    Parsing token bug fix (#6)
    * changed encryption key to whatsappnumber
    
    * working
    
    * refactoring
    
    * removed constants from router
    
    * added load env in redis
    
    * bug fix

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,15 @@
 package main
 
+import (
+	"fmt"
+	"github.com/joho/godotenv"
+)
+
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		fmt.Println("Error loading Env", err)
+	}
 	app := InitializeApp()
 	app.Start()
 }
