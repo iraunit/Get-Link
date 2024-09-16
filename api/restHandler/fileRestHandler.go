c98dfee41570e498a4627a8185ac6692ab481658
Raunit Verma <ramgopal10258@gmail.com>
2024-09-17 05:24:23 +0530

    added options

--- api/restHandler/fileRestHandler.go
+++ api/restHandler/fileRestHandler.go
@@ -94,6 +94,14 @@ func (impl *FileHandlerImpl) UploadFile(w http.ResponseWriter, r *http.Request) 
 }
 
 func (impl *FileHandlerImpl) ListAllFiles(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodOptions {
+		w.Header().Set("Access-Control-Allow-Origin", "https://codingkaro.in")
+		w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Authorization")
+		w.Header().Set("Access-Control-Allow-Methods", "POST, DELETE, GET, OPTIONS, HEAD")
+		w.WriteHeader(http.StatusNoContent)
+		return
+	}
+
 	email := muxContext.Get(r, "email").(string)
 
 	var allFiles []bean.FileInfo
